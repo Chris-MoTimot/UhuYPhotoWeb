13bf131c5259b3670d0e075260996c4f1034aedd
MayTimo <ch.timothymalelak196@gmail.com>
2026-02-02 08:27:49 +0700

    Fix layout

--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -261,6 +261,13 @@
 </div>
 
 <style>
+/* Pinterest-style Masonry Grid */
+.pinterest-grid {
+    column-count: 5;
+    column-gap: 1.5rem;
+    column-fill: balance;
+}
+
 @media (max-width: 1536px) {
     .pinterest-grid { column-count: 4; }
 }
